e6ce8f65ea80fe7e38263f57c4e9607945f14657
ryan-crabbe <128659760+ryan-crabbe@users.noreply.github.com>
2025-12-08 13:52:52 -0800

    nest in mem cache installed, registered cache globally in server

--- alcohol-labeler-backend/src/app.module.ts
+++ alcohol-labeler-backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LabelValidationModule } from './label-validation/label-validation.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [LabelValidationModule],
+  imports: [CacheModule.register({
+    isGlobal: true
+  }), LabelValidationModule],
   controllers: [AppController],
   providers: [AppService],
 })
